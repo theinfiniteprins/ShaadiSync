add4b7a733e142e34a9efe6e907aa73bd3e09094
PRINS <prinsbhuva2004@gmail.com>
2025-02-19 01:12:51 +0530

    F:Add:Add Service and Service manage

--- frontend/src/configs/config.js
+++ frontend/src/configs/config.js
@@ -2,7 +2,7 @@
 const config = {
     baseUrl: 'http://localhost:5000', // Replace with your actual backend URL,
     CLOUD_NAME : 'datwzfboc', //In this app cluodnary is used for cloud storage,
-    UPLOAD_PRESET_PROPERTY : "YOUR_UPLOAD_PRESET_FOR_PROPERTY",
+    UPLOAD_PRESET_SERVICE : "SS_Services",
     UPLOAD_PRESET_PROFILE : 'SS_UserProfile'   
 };
 
