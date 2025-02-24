cc3bf6e8a5c99265be1741b1c35eb9ea07078b80
PRINS <prinsbhuva2004@gmail.com>
2025-02-24 17:50:41 +0530

    F: Implement Artist Transactions page and update routes; modify transaction fetching logic

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -4,7 +4,8 @@ import Dashboard from "../pages/Admin/Dashboard";
 import AllUsers from "../pages/Admin/AllUsers";
 import AllArtists from "../pages/Admin/AllArtists";
 import UserTransactionHistory from "../pages/Admin/UserTransactions";
-
+import Services from "../pages/Admin/AllServices";
+import ArtistTransaction from "../pages/Admin/ArtistTransactions";
 
 
 const AdminRoutes = () => {
@@ -15,8 +16,10 @@ const AdminRoutes = () => {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/users" element={<AllUsers />} />
                 <Route path="/artists" element={<AllArtists />} />
+                <Route path="/services" element={<Services />} />
                 <Route path="/user-transactions" element={<UserTransactionHistory />} />
-               
+                <Route path="/artist-transactions" element = {<ArtistTransaction />} />
+                
             </Route>
         </Routes>
     );
