a28322ebbb25173c62648f17a7ed6618811260db
PRINS <prinsbhuva2004@gmail.com>
2025-03-15 12:27:00 +0530

    F:Update base URL for API and adjust service endpoint; fix import case for Others component

--- frontend/src/configs/config.js
+++ frontend/src/configs/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    baseUrl: 'http://localhost:5000', // Replace with your actual backend URL,
+    baseUrl: 'https://shaadisync-backend.vercel.app', // Replace with your actual backend URL,
     CLOUD_NAME : 'datwzfboc', //In this app cluodnary is used for cloud storage,
     UPLOAD_PRESET_SERVICE : "SS_Services",
     UPLOAD_PRESET_PROFILE : 'SS_UserProfile'   
